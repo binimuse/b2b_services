19dc3fc703aef38abba86fef26aa04b01dfb5548
abeyshiferaw0 <abeyshiferaw0@gmail.com>
2022-10-07 18:27:45 +0300

    distributer module => delivred page completed

--- lib/app/modules_distributer/delivred_page_distributer/views/delivred_page_distributer_view.dart
+++ lib/app/modules_distributer/delivred_page_distributer/views/delivred_page_distributer_view.dart
@@ -1,3 +1,7 @@
+import 'package:b2b_services/app/config/theme/custom_sizes.dart';
+import 'package:b2b_services/app/config/utils/pages_util.dart';
+import 'package:b2b_services/app/modules_distributer/arrived_page_distributer/views/widgets/item_order.dart';
+import 'package:b2b_services/app/modules_distributer/delivred_page_distributer/views/widgets/item_delivered_item.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -11,13 +15,30 @@ class DelivredPageDistributerView
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DelivredPageDistributerView'),
-        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle: PagesUtil.getAppBarLightStyle(),
+        toolbarHeight: 0.0,
       ),
-      body: Center(
-        child: Text(
-          'DelivredPageDistributerView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        child: ListView.separated(
+          padding: EdgeInsets.symmetric(horizontal: CustomSizes.mp_w_4),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: index == 00
+                  ? EdgeInsets.only(top: CustomSizes.mp_v_2)
+                  : EdgeInsets.zero,
+              child: ItemDelivredItem(
+                onTap: () {},
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(
+              height: CustomSizes.mp_w_2,
+            );
+          },
+          itemCount: 12,
         ),
       ),
     );
---
+++ lib/app/modules_distributer/delivred_page_distributer/views/widgets/item_delivered_item.dart
@@ -0,0 +1,268 @@
+import 'package:b2b_services/app/common/widgets/custom_button_feedback.dart';
+import 'package:b2b_services/app/config/theme/custom_colors.dart';
+import 'package:b2b_services/app/config/theme/custom_sizes.dart';
+import 'package:b2b_services/app/config/utils/color_util.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get_cli/common/utils/json_serialize/json_ast/json_ast.dart';
+
+class ItemDelivredItem extends StatefulWidget {
+  const ItemDelivredItem({Key? key, required this.onTap}) : super(key: key);
+
+  final VoidCallback onTap;
+
+  @override
+  State<ItemDelivredItem> createState() => _ItemDelivredItemState();
+}
+
+class _ItemDelivredItemState extends State<ItemDelivredItem> {
+  ///
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: CustomColors.white,
+      borderRadius: BorderRadius.circular(CustomSizes.radius_6),
+      elevation: 4,
+      shadowColor: CustomColors.black.withOpacity(0.2),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: CustomSizes.mp_w_4,
+          vertical: CustomSizes.mp_w_2,
+        ),
+        child: Column(
+          children: [
+            ///BUILD MAIN ITEM VIEW
+            buildItemView(context),
+
+            ///BUILD COLLAPSABLE EXPANDABLE VIEW
+            isSelected?buildExpandableView():SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildItemView(BuildContext context) {
+    return Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ///BUILD ICON CONTAINER
+              buildIcon(),
+
+              SizedBox(
+                width: CustomSizes.mp_w_4,
+              ),
+
+              ///BUILD ITEM INFO
+              buildItemInfo(context),
+            ],
+          );
+  }
+
+  Column buildExpandableView() {
+    return Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListView.separated(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 3,
+                padding: EdgeInsets.only(left: CustomSizes.mp_w_6,top: CustomSizes.mp_v_2),
+                itemBuilder: (context, index) {
+                  return buildDetailsRow(context);
+                },
+                separatorBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: CustomSizes.mp_v_1,
+                    ),
+                    child: const Divider(
+                      height: 1,
+                      color: CustomColors.grey,
+                    ),
+                  );
+                },
+              ),
+              SizedBox(
+                height: CustomSizes.mp_v_1,
+              ),
+            ],
+          );
+  }
+
+  Material buildIcon() {
+    return Material(
+      color: CustomColors.green.withOpacity(0.1),
+      borderRadius: BorderRadius.circular(CustomSizes.radius_8),
+      child: Padding(
+        padding: EdgeInsets.all(
+          CustomSizes.mp_w_6,
+        ),
+        child: Icon(
+          FontAwesomeIcons.layerGroup,
+          size: CustomSizes.icon_size_8,
+          color: CustomColors.green,
+        ),
+      ),
+    );
+  }
+
+  buildItemInfo(context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "Order #8769",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: CustomColors.black,
+                        fontSize: CustomSizes.font_10,
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+              ),
+              Material(
+                color: CustomColors.green.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(CustomSizes.radius_6 * 6),
+                elevation: 0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: CustomSizes.mp_w_4,
+                    vertical: CustomSizes.mp_v_1 / 2,
+                  ),
+                  child: Text(
+                    "2-items",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: ColorUtil.darken(CustomColors.green, 0.1),
+                          fontSize: CustomSizes.font_8,
+                          fontWeight: FontWeight.w600,
+                        ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: CustomSizes.mp_v_1 / 2,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                FontAwesomeIcons.solidLocationPin,
+                color: CustomColors.blue,
+                size: CustomSizes.icon_size_4,
+              ),
+              SizedBox(
+                width: CustomSizes.mp_w_1,
+              ),
+              Text(
+                "5 kilo",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: CustomColors.black,
+                      fontWeight: FontWeight.w400,
+                    ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: CustomSizes.mp_v_1,
+          ),
+          Row(
+            children: [
+              CustomButtonFeedBack(
+                onTap: () {
+                  setState(() {
+                    isSelected=!isSelected;
+                  });
+                },
+                child: Padding(
+                  padding:  EdgeInsets.all(CustomSizes.mp_w_1),
+                  child: Text(
+                    "Items Details",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: ColorUtil.darken(CustomColors.blue, 0.2),
+                          fontWeight: FontWeight.w500,
+                      fontSize: CustomSizes.font_10,
+                        ),
+                  ),
+                ),
+              ),
+              const Expanded(child: SizedBox()),
+              Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.solidLocationPin,
+                    size: CustomSizes.icon_size_4,
+                    color: CustomColors.blue,
+                  ),
+                  SizedBox(width: CustomSizes.mp_w_1,),
+                  Text(
+                    "6kilo",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: ColorUtil.darken(CustomColors.blue, 0.2),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: CustomSizes.mp_w_1,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: CustomSizes.mp_v_1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildDetailsRow(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Text(
+            "variModel.attributename",
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              color: CustomColors.grey,
+              fontWeight: FontWeight.w400,
+              fontSize: CustomSizes.font_8,
+            ),
+          ),
+        ),
+        const Expanded(
+          child: SizedBox(),
+        ),
+        Text(
+          "5l",
+          style: Theme.of(context).textTheme.bodySmall?.copyWith(
+            fontWeight: FontWeight.w500,
+            color: CustomColors.grey,
+            fontSize: CustomSizes.font_10,
+          ),
+        ),
+      ],
+    );
+  }
+}
